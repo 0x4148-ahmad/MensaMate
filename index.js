670ca53c64c50cea487e6e7effa3047c8156ed30
Ahmad Hussein <ahmad@macbook-ahmad.local>
2026-05-06 17:46:06 +0200

    chore: setup for database and switch project to ESM

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-require('dotenv').config();
+import 'dotenv/config'
+import { Telegraf } from 'telegraf'
+import Database from 'better-sqlite3';
 
-const { Telegraf } = require('telegraf');
-
+const db = new Database('canteens.db');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => {
-  return ctx.reply('MensaMate am Start! Ich höre dich.');
+db.exec('CREATE TABLE IF NOT EXISTS canteens (id INTEGER PRIMARY KEY, name TEXT NOT NULL, city TEXT NOT NULL)');
+const check = db.prepare('SELECT COUNT(*) AS count FROM canteens').get();
+
+if (check.count === 0) {
+  const canteen = await fetch('https://openmensa.org/api/v2/canteens/98');
+  const data = await canteen.json();
+
+  console.log(data)
+}
+
+
+
+
+bot.command('start',(ctx) => {
+  return ctx.reply('MensaMate am Start! Ich höre dich...');
 });
 
 bot.launch();
